b9e832772395743d6ec1b2d2c117d603d9278eed
SeerLite <seerlite@tuta.io>
2021-05-13 20:06:25 -0400

    Simplify AFK checking

--- src/js/input-controller.js
+++ src/js/input-controller.js
@@ -612,6 +612,7 @@ $("#wordsInput").keydown(function (event) {
 
   TestStats.recordKeypressSpacing();
   TestStats.setKeypressDuration(performance.now());
+  TestStats.setKeypressNotAfk();
 
   if (event.key === "Backspace" && TestLogic.input.currentWord.length === 0) {
     backspaceToPrevious();
@@ -642,51 +643,6 @@ $("#wordsInput").keydown(function (event) {
       ]
     ).toggleClass("dead");
     return;
-  }
-
-  if (
-    [
-      "ContextMenu",
-      "Escape",
-      "Shift",
-      "Control",
-      "Meta",
-      "Alt",
-      "AltGraph",
-      "CapsLock",
-      "Backspace",
-      "PageUp",
-      "PageDown",
-      "Home",
-      "ArrowUp",
-      "ArrowLeft",
-      "ArrowRight",
-      "ArrowDown",
-      "OS",
-      "Insert",
-      "Home",
-      "Undefined",
-      "Control",
-      "Fn",
-      "FnLock",
-      "Hyper",
-      "NumLock",
-      "ScrollLock",
-      "Symbol",
-      "SymbolLock",
-      "Super",
-      "Unidentified",
-      "Process",
-      "Delete",
-      "KanjiMode",
-      "Pause",
-      "PrintScreen",
-      "Clear",
-      "End",
-      undefined,
-    ].includes(event.key)
-  ) {
-    TestStats.incrementKeypressMod();
   }
 
   if (Config.layout !== "default") {
